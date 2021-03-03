a20474bcb54a84cf73e737885845ec1434e69199
hausberger_jani <hausberger.janos@codeyard.eu>
2021-03-03 21:05:22 +0100

    Hilt Dependency Injection

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -15,8 +15,15 @@ import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var someRandomString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
