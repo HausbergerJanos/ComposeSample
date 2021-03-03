de134cf7ae7179cffc84856a39a360b8e825a58d
hausberger_jani <hausberger.janos@codeyard.eu>
2021-03-03 22:54:13 +0100

    RecyclerView and CardView (LazyColumn and Card)

--- app/src/main/java/com/example/composesample/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/composesample/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
@@ -19,6 +20,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.composesample.R
+import com.example.composesample.presentation.components.RecipeCard
 import com.example.composesample.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,25 +37,12 @@ class RecipeListFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 val recipes = viewModel.recipes.value
-
-                for (recipe in recipes) {
-                    Log.d(TAG, "onCreateView: ${recipe.title}")
-                }
-
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Recipe List",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(21)
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(
-                        onClick = {
-                            findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment)
-                        }
-                    ) {
-                        Text(text = "TO RECIPE FRAGMENT")
+                
+                LazyColumn {
+                    itemsIndexed(
+                        items = recipes
+                    ) { index, recipe ->
+                        RecipeCard(recipe = recipe, onClick = {})
                     }
                 }
             }
