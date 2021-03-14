ca58d79c334d524b33a33f7c0d635bcba6d4c1fc
hausberger_jani <hausberger.janos@codeyard.eu>
2021-03-14 17:21:30 +0100

    Constraint Layout

--- app/src/main/java/com/example/composesample/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/example/composesample/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,30 +1,50 @@
 package com.example.composesample.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.WithConstraints
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.ui.tooling.preview.Preview
 
 @Composable
 fun CircularIndeterminateProgressBar(
     isDisplayed: Boolean
 ) {
     if (isDisplayed) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.Center
+        WithConstraints(
+            modifier = Modifier.fillMaxSize()
         ) {
-            CircularProgressIndicator(
-                color = MaterialTheme.colors.primary
-            )
+            val constraints = myConstraints(0.3f)
+
+            ConstraintLayout(
+                modifier = Modifier.fillMaxSize(),
+                constraintSet = constraints
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.layoutId("progressBar"),
+                    color = MaterialTheme.colors.primary
+                )
+            }
+        }
+    }
+}
+
+private fun myConstraints(verticalBias: Float): ConstraintSet {
+    return ConstraintSet {
+        val guideline = createGuidelineFromTop(verticalBias)
+        val progressBar = createRefFor("progressBar")
+
+        constrain(progressBar) {
+            top.linkTo(guideline)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
         }
     }
 }
